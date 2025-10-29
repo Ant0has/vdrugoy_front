60a4f56b56ad5f97a46bc2522c41448ee83b6efb
TrocyukS96 <trotzuk.stanislav@gmail.com>
2025-10-29 15:42:15 +0300

    fix: add next config

---
+++ next.config.js
@@ -0,0 +1,29 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'vdrugoygorod.ru',
+        port: '',
+        pathname: '**',
+      },
+    ],
+  },
+  experimental: {
+    optimizePackageImports: ['heavy-package'],
+  },
+
+  // Добавьте форматы для автоматической конвертации PNG в современные форматы
+  formats: ['image/webp', 'image/avif'],
+
+  compiler: {
+    removeConsole: process.env.NEXT_PUBLIC_STAGE === 'production',
+  },
+
+  output: 'standalone',
+  compress: true,
+  swcMinify: true,
+}
+
+module.exports = nextConfig
